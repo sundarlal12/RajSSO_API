b2828a0ae0c493b7090e5ef2a02a251d664a9d3d
achhishiksha <pardhandhamu@gmail.com>
2025-11-10 03:17:54 +0530

    first commit1

--- api/index.py
+++ api/index.py
@@ -1,10 +1,16 @@
-from raj_salary import app
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+# from raj_salary import app
+# from fastapi import FastAPI
+# from fastapi.responses import JSONResponse
 
-# Vercel uses a function handler
-def handler(event, context):
-    from mangum import Mangum
-    asgi_handler = Mangum(app)
-    return asgi_handler(event, context)
+# # Vercel uses a function handler
+# def handler(event, context):
+#     from mangum import Mangum
+#     asgi_handler = Mangum(app)
+#     return asgi_handler(event, context)
 
+# api/index.py
+from mangum import Mangum
+from raj_salary import app   # your FastAPI app instance
+
+# Vercel calls this handler per request
+handler = Mangum(app)
